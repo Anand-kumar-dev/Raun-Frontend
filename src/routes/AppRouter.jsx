e08bb42d41e58059ad1d864b69d3bf163e933a18
Anand Kumar <anand024601@gmail.com>
2025-11-05 13:37:07 +0530

    all stuff

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createBrowserRouter } from "react-router-dom";
+import App from '../App';
+import Authlayout from '../layout/Authlayout';
+
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+
+export const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Authlayout/>,
+      children:[
+        { path:"/login", element: <Login />},
+        { path:"/signup", element: <Signup />}
+        ]
+    }
+]);
+
